deeb8e9af24fa5da11dae0d61bc738b255b26c51
Vince <starryblue7@gmail.com>
2021-10-11 23:37:59 -0700

    add comments

--- assets/script.js
+++ assets/script.js
@@ -1,11 +1,12 @@
+// Load particles and configuration from particles.json
 particlesJS.load('particles-js', 'assets/particles.json', function() {
     console.log('callback - particles.js config loaded');
 });
 
+// Sticky navbar behavior
 const offset = $('#navbar').offset();
 
 window.onscroll = function() {
-    
     if (window.pageYOffset >= offset.top) {
         $('#navbar').addClass('sticky-nav');
     } else {
