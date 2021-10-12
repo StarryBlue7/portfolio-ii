1d2dc0261d6728c3e8507003e201f50e9c947634
Vince <starryblue7@gmail.com>
2021-10-11 21:59:40 -0700

    add sticky navbar

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,14 @@
 particlesJS.load('particles-js', 'assets/particles.json', function() {
     console.log('callback - particles.js config loaded');
 });
+
+const offset = $('#navbar').offset();
+
+window.onscroll = function() {
+    
+    if (window.pageYOffset >= offset.top) {
+        $('#navbar').addClass('sticky-nav');
+    } else {
+        $('#navbar').removeClass('sticky-nav');
+    }
+}
